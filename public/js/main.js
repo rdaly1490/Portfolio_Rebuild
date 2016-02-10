d95578fd041569e913c2c5d5cec8eb6f5a5eb04b
Rob <rdaly1490@gmail.com>
2016-02-09 21:49:31 -0600

    good from 300px and up

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,1 @@
-var testFile = require('./test');
-
-console.log(testFile);
-
-const blah = `${testFile.test} ES6`;
-
-const fn = (name='Rob') => {
-	console.log(`${blah} functionality now, ${name}`);
-};
-
-fn();
+var $ = require('jquery');
