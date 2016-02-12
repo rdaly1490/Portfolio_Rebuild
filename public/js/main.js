76389ae7fe502103b99a63d73c077aa7d2d18d2a
Rob <rdaly1490@gmail.com>
2016-02-11 21:12:28 -0600

    mostly finish contact, problem with vertical responsiveness

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
-var $ = require('jquery');
+// var $ = require('jquery');
 
-const doc = document.documentElement;
-const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+// const doc = document.documentElement;
+// const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
 
-if (top === 0) {
-	$('#main-page').on('scroll', function() {
-		$('#about-page').scrollIntoView();
-	});
-}
-console.log(top);
+// if (top === 0) {
+// 	$('#main-page').on('scroll', function() {
+// 		$('#about-page').scrollIntoView();
+// 	});
+// }
+// console.log(top);
