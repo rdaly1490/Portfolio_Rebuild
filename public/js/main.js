1150072d141039ec61c4941fe46a44bc7450960f
Rob <rdaly1490@gmail.com>
2016-02-13 12:15:54 -0600

    adding carouse for projects

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,44 @@
 var $ = require('jquery');
+var slick = require('slick-carousel');
 
-// const doc = document.documentElement;
-// const top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+$(document).ready(function(){
+	$('.work-carousel').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: "<img id='prev-arrow' class='a-left control-c prev slick-prev' src='/images/prev.png'>",
+		nextArrow: "<img id='next-arrow' class='a-right control-c next slick-next' src='/images/next.png'>",
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: true,
+		
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+		
+			  	}
+			}
+		]
+	});
 
-// if (top === 0) {
-// 	$('#main-page').on('scroll', function() {
-// 		$('#about-page').scrollIntoView();
-// 	});
-// }
-// console.log(top);
+	$('#prev-arrow').on('mouseenter', (e) => {
+		e.target.src = '/images/prev-hover.png';
+	});
+	$('#prev-arrow').on('mouseleave', (e) => {
+		e.target.src = '/images/prev.png';
+	});
+	$('#next-arrow').on('mouseenter', (e) => {
+		e.target.src = '/images/next-hover.png';
+	});
+	$('#next-arrow').on('mouseleave', (e) => {
+		e.target.src = '/images/next.png';
+	});
+
+});
