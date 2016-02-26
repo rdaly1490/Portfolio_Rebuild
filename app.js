88c33452675e1d32e10c85b6f5585f07353780af
Rob <rdaly1490@gmail.com>
2016-02-25 18:04:31 -0600

    enable gzip

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var compress = require('compression');
 var appRoutes = require('./routes/routes');
 
 var app = express();
@@ -8,6 +8,7 @@ app.engine('html', require('hogan-express'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 
+app.use(compress());
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/node_modules'));
 
