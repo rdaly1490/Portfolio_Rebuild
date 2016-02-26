1020b2859ac675c2b153c0d57befb32c771e3399
Rob <rdaly1490@gmail.com>
2016-02-25 19:10:45 -0600

    basic seo and linked in icon

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ module.exports = function(express, app) {
 	var router = express.Router();
 
 	router.get('/', function(req, res, next) {
-		res.render('index', {title: 'DalyWebDev'});
+		res.render('index', {title: 'DalyWebDev | Freelance Web Developer'});
 	});
 
 	app.use('/', router);
